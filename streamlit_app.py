88c1ff1bcdb3573bb1d4b6cd78969bd96bfb4488
Mateusz Tchórzewski <159627124+mateusztch@users.noreply.github.com>
2024-10-03 19:54:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,72 +29,73 @@ st.set_page_config(page_title="🎉 Asystent Wieczoru Kawalerskiego Huberta", pa
 #        else:
 #            st.error("Nieprawidłowe hasło, spróbuj ponownie.")
 
-#if st.session_state['authorized']:
-    # Title and initial setup if authorized
-    st.title("🎉 Asystent Wieczoru Kawalerskiego Santy")
-    st.write(
-        "Prośba o zadanie do max 20 zapytań bo API jest płatne wariaty."
-    )
+# Usunięto warunek if st.session_state['authorized']: i odpowiednio dostosowano wcięcia poniższego kodu
 
-    # Importing API
-    openai_api_key = st.secrets["OPENAI_API_KEY"]
+# Title and initial setup if authorized
+st.title("🎉 Asystent Wieczoru Kawalerskiego Santy")
+st.write(
+    "Prośba o zadanie do max 20 zapytań bo API jest płatne wariaty."
+)
 
-    # Initiating LLM
-    llm = ChatOpenAI(
-        openai_api_key=openai_api_key,
-        model_name="gpt-3.5-turbo",
-        temperature=0.4  # Ustaw na 0 dla bardziej deterministycznych odpowiedzi
-    )
+# Importing API
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-    # Google Sheets API
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    credentials_info = st.secrets["google_credentials"]
-    creds = Credentials.from_service_account_info(credentials_info, scopes=scope)
-    client = gspread.authorize(creds)
+# Initiating LLM
+llm = ChatOpenAI(
+    openai_api_key=openai_api_key,
+    model_name="gpt-3.5-turbo",
+    temperature=0.4  # Ustaw na 0 dla bardziej deterministycznych odpowiedzi
+)
 
-    # Loading Google Sheets data
-    spreadsheet = client.open('Kawalerski')
-    sheet1 = spreadsheet.worksheet('Co zabrać')
-    sheet2 = spreadsheet.worksheet('Plan wyjazdu')
-    sheet3 = spreadsheet.worksheet('Koszta')
-    sheet4 = spreadsheet.worksheet('Q&A')
+# Google Sheets API
+scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+credentials_info = st.secrets["google_credentials"]
+creds = Credentials.from_service_account_info(credentials_info, scopes=scope)
+client = gspread.authorize(creds)
 
-    # Converting to DataFrames
-    df1 = pd.DataFrame(sheet1.get_all_records())
-    df2 = pd.DataFrame(sheet2.get_all_records())
-    df3 = pd.DataFrame(sheet3.get_all_records())
-    df4 = pd.DataFrame(sheet4.get_all_records())
-    dfs = [(df1, 'Co zabrać'), (df2, 'Plan wyjazdu'), (df3, 'Koszta'), (df4, 'Q&A')]
+# Loading Google Sheets data
+spreadsheet = client.open('Kawalerski')
+sheet1 = spreadsheet.worksheet('Co zabrać')
+sheet2 = spreadsheet.worksheet('Plan wyjazdu')
+sheet3 = spreadsheet.worksheet('Koszta')
+sheet4 = spreadsheet.worksheet('Q&A')
 
-    # Creating list of docs
-    documents = []
+# Converting to DataFrames
+df1 = pd.DataFrame(sheet1.get_all_records())
+df2 = pd.DataFrame(sheet2.get_all_records())
+df3 = pd.DataFrame(sheet3.get_all_records())
+df4 = pd.DataFrame(sheet4.get_all_records())
+dfs = [(df1, 'Co zabrać'), (df2, 'Plan wyjazdu'), (df3, 'Koszta'), (df4, 'Q&A')]
 
-    # Iterate over each DataFrame
-    for df, sheet_name in dfs:
-        sheet_str = f"Arkusz: {sheet_name}.\n"
-        for index, row in df.iterrows():
-            row_str = ' '.join(f"{col_name}: {row[col_name]}" for col_name in df.columns)
-            sheet_str += row_str + '\n'
-        documents.append(sheet_str)
+# Creating list of docs
+documents = []
 
-    # Creating chunks
-    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-    texts = []
-    for doc in documents:
-        texts.extend(text_splitter.split_text(doc))
+# Iterate over each DataFrame
+for df, sheet_name in dfs:
+    sheet_str = f"Arkusz: {sheet_name}.\n"
+    for index, row in df.iterrows():
+        row_str = ' '.join(f"{col_name}: {row[col_name]}" for col_name in df.columns)
+        sheet_str += row_str + '\n'
+    documents.append(sheet_str)
 
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    vectorstore = FAISS.from_texts(texts, embeddings)
+# Creating chunks
+text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+texts = []
+for doc in documents:
+    texts.extend(text_splitter.split_text(doc))
 
-    # Creating memory
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", return_messages=True
-    )
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+vectorstore = FAISS.from_texts(texts, embeddings)
 
-    # Prompt template
-    prompt_template = PromptTemplate(
-        input_variables=["context", "question"],
-        template="""
+# Creating memory
+memory = ConversationBufferMemory(
+    memory_key="chat_history", return_messages=True
+)
+
+# Prompt template
+prompt_template = PromptTemplate(
+    input_variables=["context", "question"],
+    template="""
 Jako asystent wieczoru kawalerskiego Huberta, pomóż odpowiedzieć na pytania związane z wyjazdem. Używaj dostarczonych danych z Google Sheets, zwracając szczególną uwagę na nazwy arkuszy i nagłówki kolumn, aby zrozumieć kontekst. Używaj młodzieżowego i swobodnego tonu wypowiedzi, zachowując się przy tym miło. Odpowiadaj na pytania tylko po polsku.
 
 Jeśli nie znasz odpowiedzi na pytanie na podstawie dostępnych danych, grzecznie i krótko poinformuj użytkownika, że na ten moment nie znasz odpowiedzi i poproś o inne pytanie.
@@ -111,48 +112,48 @@ Kontekst: {context}
 Pytanie: {question}
 
 Odpowiedź:
-        """
-    )
+    """
+)
 
-    # Chain
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(search_kwargs={"k": 10}),
-        memory=memory,
-        verbose=False,
-        combine_docs_chain_kwargs={"prompt": prompt_template}
-    )
+# Chain
+qa = ConversationalRetrievalChain.from_llm(
+    llm=llm,
+    retriever=vectorstore.as_retriever(search_kwargs={"k": 10}),
+    memory=memory,
+    verbose=False,
+    combine_docs_chain_kwargs={"prompt": prompt_template}
+)
 
-    # Conversation history
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "Siemanko! Z przyjemnością odpowiem wam na pytania dotyczące wyjazdu :)"}]
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Conversation history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Siemanko! Z przyjemnością odpowiem wam na pytania dotyczące wyjazdu :)"}]
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Chat input
-    if prompt := st.chat_input("Podaj pytanie dotyczące wieczoru kawalerskiego Santy:"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+# Chat input
+if prompt := st.chat_input("Podaj pytanie dotyczące wieczoru kawalerskiego Santy:"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        # Chain response
-        with st.spinner("Szukam odpowiedzi..."):
-            try:
-                response = qa({"question": prompt})
-                answer = response['answer']
-                sources = response.get('source_documents', [])
+    # Chain response
+    with st.spinner("Szukam odpowiedzi..."):
+        try:
+            response = qa({"question": prompt})
+            answer = response['answer']
+            sources = response.get('source_documents', [])
 
-                st.session_state.messages.append({"role": "assistant", "content": answer})
-                with st.chat_message("assistant"):
-                    st.markdown(answer)
+            st.session_state.messages.append({"role": "assistant", "content": answer})
+            with st.chat_message("assistant"):
+                st.markdown(answer)
 
-            except RateLimitError:
-                st.error("Przekroczyłeś limit szybkości API OpenAI. Sprawdź swoje szczegóły planu i rozliczeń.")
-            except Exception as e:
-                st.error("Wystąpił nieoczekiwany błąd podczas przetwarzania Twojego pytania. Spróbuj ponownie później.")
+        except RateLimitError:
+            st.error("Przekroczyłeś limit szybkości API OpenAI. Sprawdź swoje szczegóły planu i rozliczeń.")
+        except Exception as e:
+            st.error("Wystąpił nieoczekiwany błąd podczas przetwarzania Twojego pytania. Spróbuj ponownie później.")
 
-    # Clearing chat history button
-    if st.button("Wyczyść historię czatu"):
-        st.session_state.messages = []
-        st.experimental_rerun()
+# Clearing chat history button
+if st.button("Wyczyść historię czatu"):
+    st.session_state.messages = []
+    st.experimental_rerun()
